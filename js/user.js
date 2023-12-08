24a56a65bf5e2d10c200d564dbe9dc974b27feab
Aday <adaymarfil89@gmail.com>
2023-12-09 00:30:30 +0200

    ADD + MOD: Fonts + More small Changes

---
+++ js/user.js
@@ -0,0 +1,10 @@
+//I wanna create a function of myself :) 
+//_Mood, _Mode, _Style, _PhisicalStrength, _MentalStrength, _Feeling, _Pain, _CheckPoint(return bool, parameters DateTime & proceeded), _UserName, _MusicStyle, _TotalPoints(Confidence, Friendship, Sociality, SPECIAL), _Emotion, _Status, _StatusCode, _Flawless%, _SelfPerception, _HornyLevel
+//Change CRUD for List<String>
+
+class User{
+    constructor(Name, age){
+
+    }
+    // this.Name;
+}
