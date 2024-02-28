d642ff6d1e1a539a7dab65217230d370429b9460
Aday <adaymarfil89@gmail.com>
2024-02-28 13:52:41 +0200

    MOD: Hacker Style in Gallery

--- js/changeLayout.js
+++ js/changeLayout.js
@@ -43,6 +43,16 @@ function VerifingNewLayout(newLayout){
             newLayoutName = "retro";
             break;
 
+        //Gallery Layout
+
+        case "gallery_retro":
+            NoCSS();
+            BootstrapCSS();
+            HackerGalleryCSS();
+            console.log("STOP HACKING ME! I'M SCARED!! PLS, I'LL DO ANYTHING!!!");
+            newLayoutName = "gallery_retro";
+            break;
+
 
         default:
             newLayoutName = "trolling"; //do the barrel roll? idk
@@ -97,6 +107,41 @@ function HackerCSS() {
     let link_4 = document.createElement("link");
     let link_5 = document.createElement("link");
     let link_6 = document.createElement("link");
+    let link_7 = document.createElement("link");
+
+    link_1.setAttribute("rel", "stylesheet");
+    link_2.setAttribute("rel", "stylesheet");
+    link_3.setAttribute("rel", "stylesheet");
+    link_4.setAttribute("rel", "stylesheet");
+    link_5.setAttribute("rel", "stylesheet");
+    link_6.setAttribute("rel", "stylesheet");
+    link_7.setAttribute("rel", "stylesheet");
+
+    link_1.setAttribute("href", "/css/retro/retro.css");
+    link_2.setAttribute("href", "/css/retro/font/font.css");
+    link_3.setAttribute("href", "/css/retro/header/header.css");
+    link_4.setAttribute("href", "/css/retro/main/main.css");
+    link_5.setAttribute("href", "/css/retro/footer/footer.css");
+    link_6.setAttribute("href", "/css/retro/cursor/cursor.css");
+    link_7.setAttribute("href", "/css/retro/images.css");
+
+    document.head.appendChild(link_1);
+    document.head.appendChild(link_2);
+    document.head.appendChild(link_3);
+    document.head.appendChild(link_4);
+    document.head.appendChild(link_5);
+    document.head.appendChild(link_6);
+    document.head.appendChild(link_7);
+}
+
+function HackerGalleryCSS() {
+    let link_1 = document.createElement("link");
+    let link_2 = document.createElement("link");
+    let link_3 = document.createElement("link");
+    let link_4 = document.createElement("link");
+    let link_5 = document.createElement("link");
+    let link_6 = document.createElement("link");
+
 
     link_1.setAttribute("rel", "stylesheet");
     link_2.setAttribute("rel", "stylesheet");
@@ -105,12 +150,15 @@ function HackerCSS() {
     link_5.setAttribute("rel", "stylesheet");
     link_6.setAttribute("rel", "stylesheet");
 
+
     link_1.setAttribute("href", "/css/retro/retro.css");
     link_2.setAttribute("href", "/css/retro/font/font.css");
     link_3.setAttribute("href", "/css/retro/header/header.css");
-    link_4.setAttribute("href", "/css/retro/main/main.css");
-    link_5.setAttribute("href", "/css/retro/footer/footer.css");
-    link_6.setAttribute("href", "/css/retro/cursor/cursor.css");
+    link_4.setAttribute("href", "/css/retro/footer/footer.css");
+    link_5.setAttribute("href", "/css/retro/cursor/cursor.css");
+
+    //Gallery Link (overide the main retro)
+    link_6.setAttribute("href", "/css/retro/gallery/gallery.css");
 
     document.head.appendChild(link_1);
     document.head.appendChild(link_2);
@@ -120,6 +168,19 @@ function HackerCSS() {
     document.head.appendChild(link_6);
 }
 
+function BootstrapCSS(){
+    let link_1 = document.createElement("link");
+
+    link_1.setAttribute("rel", "stylesheet");
+
+    //Bootstrap Link 
+    link_1.setAttribute("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
+    link_1.setAttribute("integrity", "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN");
+    link_1.setAttribute("crossorigin", "anonymous");
+
+    document.head.appendChild(link_1);
+
+}
 
 window.onload = () => {
     //Definition of all the Elements of the Web
