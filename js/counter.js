3a12e3302ad02a3f39f5c05f475cb5257cee6d15
Aday <adaymarfil89@gmail.com>
2024-04-27 01:20:51 +0300

    Rock Style in The Music Realm + Working P5Clock

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,7 @@
+//Put your Neocities Website Here. PS: You can comment (put before //) or erase all the console.log()
 async function Search(){
     const siteInfo = "https://weirdscifi.ratiosemper.com/neocities.php?sitename=";
-    const username = "user-maldito";//Place Here Your Neocities Username
+    const username = "user-maldito";  //Place Here Your Neocities Username -> Example: "cinni" or "dawsomespace"
 
     console.log("Initializing Search");
     let theInfo = await GetSiteInfo(siteInfo, username);
@@ -10,12 +11,13 @@ async function Search(){
 }
 
 async function GetSiteInfo(urlSite, username){
+    console.log("Let's Get This Site's Data/Info");
     let yourWebsiteData = {
         success: false,
         views: 0,
         hits: 0,
         created: "",
-        last_update: "",
+        updated: "",
         domain: "",
         tags: [],
     }
@@ -31,7 +33,6 @@ async function GetSiteInfo(urlSite, username){
         }
     })
     .then(data => {
-        //console.log(data);
         if (data.result == "success") {
             const separatorDate = "/";
             let created = new Date(data.info.created_at);
@@ -41,10 +42,8 @@ async function GetSiteInfo(urlSite, username){
             yourWebsiteData.name = data.info.sitename;
             yourWebsiteData.views = data.info.views;
             yourWebsiteData.hits = data.info.hits;
-            //   [dd/MM/YYYY] 24/11/2014 --- 12:36:58 (example)
-            yourWebsiteData.created = created.getDate() + separatorDate + created.getMonth() + separatorDate + created.getFullYear() + " - " + created.getHours() + ":" + created.getMinutes() + ":"  + created.getSeconds();
-            yourWebsiteData.last_update = updated.getDate() + separatorDate + updated.getMonth() + separatorDate + updated.getFullYear() + " - " + updated.getHours() + ":" + updated.getMinutes() + ":" + updated.getSeconds();
-            
+            yourWebsiteData.created = created;
+            yourWebsiteData.updated = updated;
             yourWebsiteData.domain = data.info.domain;
             yourWebsiteData.tags = data.info.tags;
 
@@ -57,16 +56,135 @@ async function GetSiteInfo(urlSite, username){
     return yourWebsiteData;
 }
 
-function SetCounterInfo(webData){
+function SetCounterInfo(webData){ // Your views = 6331 (example) | Last Update = 14/12/2018
     let viewBox = document.querySelector("#views");
     let createdBox = document.querySelector("#created");
     let updatedBox = document.querySelector("#updated");
 
-    viewBox.innerText ="My views: " + webData.views;
-    createdBox.innerText = 'Created: ' + webData.created;
-    updatedBox.innerText = `Updated: ${webData.last_update}`;
+    //let betterViews = BetterViewsInfo(webData.views); // Your Better views = 6.331
+    let bestViews = BestViewInfo(webData.views); // Your Best views = 6.3 K or 6.3 M
+
+    let betterCreatedDate = BetterDateInfo(webData.created);
+    let betterUpdatedDate = BetterDateInfo(webData.updated, 2); 
+
+    viewBox.innerText ="My views: " + bestViews;
+    createdBox.innerText = 'Created: ' + betterCreatedDate;
+    updatedBox.innerText = `Updated: ${betterUpdatedDate}`;
 
     console.log("Write it down");
 }
+
+function BetterViewsInfo(views){
+    let betterInfo ="";
+
+    let stringViews = views.toString();
+
+    if (views.toString().length >= 4){
+        betterInfo = stringViews[0] + "." + stringViews.substring(1, stringViews.length);
+    }
+    if (views.toString().length >= 7){
+        betterInfo = stringViews[0] + "." + stringViews.substring(1, stringViews.length - 4) + "." + stringViews.substring(stringViews.length - 3, stringViews.length);
+    }
+    if (views.toString().length < 4) {
+        betterInfo = views;
+    }
+
+    return betterInfo;
+}
+function BestViewInfo(views){
+    let bestInfo = "";
+    let stringViews = views.toString();
+
+    switch (true) {
+        case ((stringViews.length >= 4) && (stringViews.length < 7)):
+            bestInfo = stringViews[0] + "." + stringViews[1] + " K";
+        break;
+
+        case (stringViews.length >= 7):
+            bestInfo = stringViews[0] + "." + stringViews[1] + " M";
+        break;
+    
+        default:
+            bestInfo = stringViews;
+        break;
+    }
+
+    return bestInfo;
+}
+
+//IMPORTANT TO UNDERSTAND
+/*
+    options -> 1. onlyDays  2. onlyWeeks  3. onlyMonths other thing. Full display
+    Example: 
+        option = 1 -> Last Update = 19 day(s) ago
+        option = 2 -> Last Update = 2 week(s) ago
+        option = 3 -> Last Update = Less than a month(s) ago
+        option = 4 or 0 or nothing -> Last Update = 2 weeks and 2 day(s) ago
+*/
+function BetterDateInfo(date, options) {
+    let betterDate = "";
+    
+    let dateNow = new Date(Date.now());
+    let betterDays = Math.abs(dateNow.getDate() - date.getDate());
+    let betterMonths = Math.abs(dateNow.getMonth() - date.getMonth());
+    let betterYears = Math.abs(dateNow.getFullYear() - date.getFullYear());
+
+    let allDays = Math.floor((betterYears / 365) + (betterMonths * 30) + betterDays);
+    console.log(allDays);
+
+    switch (options) {
+        case 1:
+            if (allDays == 0) {
+                //betterDate = "Right Now";
+                betterDate = "Less than a day";
+            }
+            else{
+                betterDate = allDays + " day(s)";
+            }
+        break;
+        case 2:
+            if (allDays < 7) {
+                betterDate = "Less than a week";
+            }
+            else{
+                betterDate = Math.floor(allDays / 7) + " week(s)";
+            }
+        break;
+        case 3:
+            if (betterMonths < 1) {
+                betterDate =  "Less than a month";
+            } else {
+                betterDate = betterMonths + " month(s)";
+            }
+        break;
+        case 4:
+            if (allDays <= 365) {
+                betterDate =  "Less than a year";
+            } else {
+                let theBestYear = Math.floor(allDays / 365);
+                betterDate = theBestYear + " year(s)";
+            }
+        break;
+        default:
+            if (betterMonths > 12) {
+                betterDate = betterYears + " year(s) ";
+            }
+            if (betterMonths > 0) {
+                betterDate += betterMonths + " month(s) ";
+            }
+            if (betterDays > 0) {
+                betterDate += betterDays + " day(s)";
+            }
+            if (allDays == 0) {
+                //betterDate = "Right Now";
+                betterDate = "Less than a day";
+            }
+        break;
+    }
+
+
+    return betterDate + " ago";
+}
+
 
 Search();
